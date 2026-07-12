431cf41825702150702012f18446b17404d6f1c4
Moises Pastran <moilqp@gmail.com>
2026-07-12 10:46:05 +0200

    nothing

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <a href="https://github.com/crazypregammer/react-project">Link to the repo's project</a>
+        </footer>
+    );
+}
+
+export default Footer;
--- ecommerce/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-function Footer() {
-    return (
-        <a href="https://github.com/crazypregammer/react-project">Link</a>
-    );
-}
-
-export default Footer;
