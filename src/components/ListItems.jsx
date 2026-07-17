41d599bc1370300befc1bd71f20a71a5d9734a9a
Moises Pastran <moilqp@gmail.com>
2026-07-17 14:50:54 +0200

    styling

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,16 +1,19 @@
-function ListItems ({product, onDelete}) {
+function ListItems({ product, onDelete }) {
+  return (
+    <li className="list-item">
+      <img className="list-item-image" src={product.thumbnail} alt={product.title} />
 
-    return(
-        <li className="ListItems">
-            <img className="listIemsImage" src={product.thumbnail} alt={product.title} />
-            <h3>{product.title}</h3>
-            <p className="listItemsPrice">{product.price} €</p>
-            <p>Stock: {product.stock}</p>
-            <button onClick={() => onDelete(product.id)}>Delete</button>
-        </li>
-    );
+      <div className="list-item-info">
+        <h3>{product.title}</h3>
+        <p className="price">{product.price} €</p>
+        <p className="stock">Stock: {product.stock}</p>
+      </div>
+
+      <button className="delete-btn" onClick={() => onDelete(product.id)}>
+        Delete
+      </button>
+    </li>
+  );
 }
-
-
 
 export default ListItems;
